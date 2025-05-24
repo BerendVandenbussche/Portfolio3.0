8af4c180a28943bf95ca33cc708695b937840bfc
Berend Vandenbussche <berend@inuits.eu>
2025-05-24 22:01:59 +0200

    Feat: Split-up components and rework menu styling

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,29 @@
 import NavItem from "@/components/navigation/NavItem.vue";
 import PortfolioSection from "@/components/PortfolioSection.vue";
 import ContactItem from "@/components/contact/ContactItem.vue";
+import HomeSectionTemplate from "@/components/sectionTemplates/HomeSectionTemplate.vue";
 
 const navigationItems = ["Home", "About", "Skills", "Homelab", "Contact"];
 </script>
 
 <template>
-  <div class="bg-dark">
+  <div class="bg-dark text-white">
+    <header class="fixed top-0 bg-darkest-see-trough w-full">
+      <nav class="flex justify-center items-center p-4">
+        <div
+          class="relative mr-[2rem] flex justify-center items-center w-12 h-12 rounded-lg bg-theme font-bold"
+        >
+          <p>BV.</p>
+        </div>
+        <ul class="flex justify-center">
+          <NavItem
+            v-for="navItem in navigationItems"
+            :key="navItem"
+            :title="navItem"
+          />
+        </ul>
+      </nav>
+    </header>
     <portfolio-section
       v-for="(navigationItem, index) in navigationItems"
       :key="index"
@@ -15,39 +32,7 @@ const navigationItems = ["Home", "About", "Skills", "Homelab", "Contact"];
       :class="[index % 2 === 0 ? 'bg-darkest' : 'bg-dark', 'text-white']"
     >
       <template v-if="index === 0">
-        <header>
-          <nav>
-            <div
-              class="absolute top-2 left-2 flex justify-center items-center w-12 h-12 rounded-lg bg-theme font-bold"
-            >
-              <p>BV.</p>
-            </div>
-            <ul class="flex justify-center">
-              <NavItem
-                v-for="navItem in navigationItems"
-                :key="navItem"
-                :title="navItem"
-              />
-            </ul>
-          </nav>
-        </header>
-        <section>
-          <div class="w-full h-full flex justify-center py-12">
-            <div class="w-fit">
-              <img
-                src="/images/MeBlob.png"
-                alt="A blue colored blob with a picture of myself"
-              />
-              <div class="mx-12 -mt-24">
-                <h1 class="text-4xl">
-                  Berend<br />
-                  Vandenbussche
-                </h1>
-                <h2 class="font-normal">Frontend developer</h2>
-              </div>
-            </div>
-          </div>
-        </section>
+        <HomeSectionTemplate />
       </template>
 
       <template v-if="index === 1">
---
+++ src/components/sectionTemplates/HomeSectionTemplate.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import NavItem from "@/components/navigation/NavItem.vue";
+</script>
+
+<template>
+  <section>
+    <div class="w-full h-full flex justify-center py-12">
+      <div class="w-fit">
+        <img
+          src="/images/MeBlob.png"
+          alt="A blue colored blob with a picture of myself"
+        />
+        <div class="mx-12 -mt-24">
+          <h1 class="text-4xl">
+            Berend<br />
+            Vandenbussche
+          </h1>
+          <h2 class="font-normal">Frontend developer</h2>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
