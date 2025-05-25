814e1515bf80a7bfc5f1879770453719fef09871
Berend Vandenbussche <berend@inuits.eu>
2025-05-25 18:41:35 +0200

    Feat: Set homelab section under construction

---
+++ src/components/sectionTemplates/HomeLabSectionTemplate.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1 class="text-theme">Homelab</h1>
+    <div class="w-full h-screen flex flex-col items-center justify-center">
+      <img
+        class="h-[500px] pb-12"
+        src="/images/construction.svg"
+        alt="Under construction"
+      />
+      <h2>Under construction</h2>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
